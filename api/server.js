e8f3858c2ca07012431aa90dd4d1ab5bca65cc3d
Dharmik Savaliya <danny@Dharmiks-MacBook-Pro.local>
2021-08-30 13:33:26 -0700

    safety

--- api/server.js
+++ api/server.js
@@ -2,13 +2,14 @@
 const express = require('express')
 const User = require('./users/model')
 const server = express()
+server.use(express.json())
 
-server.get('/api/users', (req, res)=>{
+server.get('/api/users', (req, res) => {
     User.find()
-    .then(users =>{
+    .then(users => {
         res.json(users)
     })
-    .catch(err=>{
+    .catch(err => {
         res.status(500).json({
             message: 'Error getting users',
             err: err.message,
@@ -16,9 +17,9 @@ server.get('/api/users', (req, res)=>{
     })
 })
 
-server.get('/api/users/:id', (req, res)=>{
+server.get('/api/users/:id', (req, res) => {
     User.findById(req.params.id)
-    .then(user =>{
+    .then(user => {
         if(!user){
             res.status(404).json({
                 message: 'The user with the specified ID does not exist'
@@ -26,7 +27,7 @@ server.get('/api/users/:id', (req, res)=>{
         }
         res.json(user)
     })
-    .catch(err=>{
+    .catch(err => {
         res.status(500).json({
             message: 'Error getting user',
             err: err.message,
@@ -34,7 +35,65 @@ server.get('/api/users/:id', (req, res)=>{
     })
 })
 
-server.use('*', (req, res)=>{
+server.post('/api/users', (req, res) => {
+    const user =req.body
+    if(!user.name || !user.bio){
+        res.status(400).json({
+            message: 'Please provide name and bio for the user'
+        })
+    }else{
+    User.insert(user)
+    .then(newUser => {
+        res.status(201).json(newUser)
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: 'Error creating user',
+            err: err.message,
+        })
+    })
+    }
+})
+
+server.delete('/api/users/:id', (req, res) => {
+    User.remove(req.params.id)
+    .then(user => {
+        if(user){
+            res.status(200).json(user)
+        }else{
+            res.status(404).json({
+                message: "The user with the specified ID does not exist" })
+        }
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: "The user could not be removed",
+            err: err.message,
+        })
+    })
+})
+
+server.put('/api/user/:id', async (req, res) => {
+    const { id } = req.params
+    const changes = req.body
+    console.log(id, changes)
+    
+    .then(user => {
+        if(user){
+            res.status(200).json(user)
+        }else{
+            res.status(404).json({
+                message: "The user with the specified ID does not exist" })
+        }
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: "The user information could not be modified",
+            err: err.message,
+        })
+    })
+})
+server.use('*', (req, res) => {
     res.status(404).json({message: 'not found'})
 })
 
